80a2d851e33b19d06b17ff74657b660426bd3935
chirag ahir <ahirchirag1459@gmail.com>
2023-04-23 13:09:00 -0400

    latest commit

---
+++ src/main/java/com/spb/model/Customer.java
@@ -0,0 +1,12 @@
+package com.spb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Customer {
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
