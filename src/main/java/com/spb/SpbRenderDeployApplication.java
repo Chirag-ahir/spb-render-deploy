70c1100bfa2e454a060e2662e683dd39a4895f35
chirag ahir <ahirchirag1459@gmail.com>
2023-04-23 13:09:29 -0400

    latest commit

---
+++ src/main/java/com/spb/SpbRenderDeployApplication.java
@@ -0,0 +1,11 @@
+package com.spb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbRenderDeployApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpbRenderDeployApplication.class, args);
+    }
+}
